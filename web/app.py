31b07d4308e6a8d4db24f53ecd763fe1f5569150
niranjana687 <niranjana687@gmail.com>
2022-09-10 11:26:57 +0530

    add funds endpoint completed

--- web/app.py
+++ web/app.py
@@ -1,4 +1,3 @@
-from distutils.log import debug
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from pymongo import Mongoclient
@@ -24,6 +23,27 @@ def generateReturnDictionary(status, message):
         "message": message
     }
     return jsonify(returnJson)
+
+def verifyLogin(username, password):
+    if verifyUsername(username):
+        return False
+    
+    pwd = users.find_one({"Username": username})[0]["Password"]
+
+    if bcrypt.hashpw(password.endode('utf8'), bcrypt.gensalt()) != pwd:
+        return False
+    
+    return True
+        
+def checkAmount(amount):
+    if amount <= 0:
+        return False
+    
+    return True
+
+def getBalance(username):
+    balance = users.find_one({"Username":username})[0]["Balance"]
+    return balance
 
 # Register to the app
 class Register(Resource):
@@ -54,7 +74,45 @@ class Register(Resource):
 
         return generateReturnDictionary(200, "Registration successful")    
 
+class Add(Resource):
+    def post(self):
+        # get posted data
+        postedData = request.get_json()
+
+        # retrieve username, password and amount to be added
+        username = postedData["username"]
+        password = postedData["password"]
+        amount = postedData["amount"]
+
+        # verify login creds
+        verified = verifyLogin(username, password)
+
+        # if credentials are incorrect send error code amd message
+        if not verified:
+            return generateReturnDictionary(303, "incorrect login credentials")
+
+        # once verified check if the amount is greater than zero
+        positive = checkAmount(amount)
+
+        # send error message if not
+        if not positive:
+            return generateReturnDictionary(304, "amount should be greater than 0")
+
+        # else update balance 
+        balance = getBalance(username)
+
+        users.find_one({
+            "Username": username
+        }, {
+            "$set": {
+                "Balance": balance + amount
+            }
+        })
+
+        return generateReturnDictionary(200, "Successfully added funds")
+
 api.add_resource(Register, '/register')
+api.add_resource(Add, '/add')
 
 if __name__=="__main__":
     app.run('0.0.0.0', port=5000, debug=True)
